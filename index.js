02ff91da8a58e886819c71dc12ec0e66ab9e4c20
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-27 20:33:48 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record){
+let o=record.find(record => record.result === "W");
+
+if(o === undefined)
+{
+  return o
+}
+else{return o.year}
+
+}
+
